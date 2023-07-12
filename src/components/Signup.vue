3a7f9c64a7f33234214b3268301994167ea94f31
fernando <generatof@gmail.com>
2023-07-12 11:58:43 -0300

    add loading in signup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,49 +1,57 @@
 <template>
-  <form class="form-signin">
-    <img
-      class="mb-4"
-      src="https://img.icons8.com/clouds/100/enter-2.png"
-      width="100"
-      height="100"
-    />
-    <h1 class="h3 mb-3 font-weight-normal">Olá, Faça o seu cadastro abaixo.</h1>
-    <label for="inputName" class="sr-only">Nome</label>
-    <input
-      v-model="form.name"
-      type="name"
-      id="inputName"
-      class="form-control"
-      placeholder="Nome"
-      required
-    />
-    <label for="inputEmail" class="sr-only">E-mail</label>
-    <input
-      v-model="form.email"
-      type="email"
-      id="inputEmail"
-      class="form-control"
-      placeholder="E-mail"
-      autofocus=""
-      required
-    />
+  <div v-if="!apiLoading">
+    <form class="form-signin">
+      <img
+        class="mb-4"
+        src="https://img.icons8.com/clouds/100/enter-2.png"
+        width="100"
+        height="100"
+      />
+      <h1 class="h3 mb-3 font-weight-normal">
+        Olá, Faça o seu cadastro abaixo.
+      </h1>
+      <label for="inputName" class="sr-only">Nome</label>
+      <input
+        v-model="form.name"
+        type="name"
+        id="inputName"
+        class="form-control"
+        placeholder="Nome"
+        required
+      />
+      <label for="inputEmail" class="sr-only">E-mail</label>
+      <input
+        v-model="form.email"
+        type="email"
+        id="inputEmail"
+        class="form-control"
+        placeholder="E-mail"
+        autofocus=""
+        required
+      />
 
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input
-      v-model="form.password"
-      type="password"
-      id="inputPassword"
-      class="form-control"
-      placeholder="Senha"
-      required
-    />
-  </form>
-  <button @click="signup(form)" class="btn btn-lg btn-primary btn-block">
-    Cadastre-se
-  </button>
-  <button @click="backToLogin()" class="btn btn-lg btn-primary btn-block">
-    Voltar
-  </button>
-
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input
+        v-model="form.password"
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Senha"
+        required
+      />
+    </form>
+    <button @click="signup(form)" class="btn btn-lg btn-primary btn-block">
+      Cadastre-se
+    </button>
+    <button @click="backToLogin()" class="btn btn-lg btn-primary btn-block">
+      Voltar
+    </button>
+  </div>
+  <div v-else class="text-center">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
   <p class="mt-5 mb-3 text-muted">© 2023</p>
 </template>
 <script>
@@ -52,6 +60,7 @@ const sweet = require("sweetalert2");
 export default {
   data() {
     return {
+      apiLoading: false,
       form: {
         name: "",
         email: "",
@@ -79,6 +88,7 @@ export default {
       });
     },
     signup(form) {
+      this.apiLoading = true;
       AuthService.createUser(form)
         .then(() => {
           sweet.fire("Sucesso!", "Cadastro realizado!", "success");
@@ -95,7 +105,8 @@ export default {
             default:
               this.showAlert();
           }
-        });
+        })
+        .finally(() => (this.apiLoading = false));
     },
     backToLogin() {
       this.$router.push("/login");
