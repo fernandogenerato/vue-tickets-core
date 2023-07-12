83b1b6480800d820c6fd3557f02e023bc0b2095d
Fernando Generato <fernando.generato@mercadolivre.com>
2023-07-12 02:10:42 -0300

    Atualizar o http-common.js

--- http-common.js
+++ http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://go-tickets-core.onrender.com",
-   baseURL: "http://localhost:8080",
+  baseURL: "https://go-tickets-core.onrender.com",
+   // baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json"
   }
